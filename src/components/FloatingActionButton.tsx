1b8294b87adc136f0f3e2587214dba0cbde9af27
Preedroos <Preedroos@gmail.com>
2026-07-11 17:26:13 -0400

    style(FAB): altera a cor do background

--- src/components/FloatingActionButton.tsx
+++ src/components/FloatingActionButton.tsx
@@ -7,7 +7,7 @@ export function FloatingActionButton({ icon, onClick }: FloatingActionButtonProp
     return (
         <button
             onClick={onClick}
-            className="fixed bottom-6 right-6 w-14 h-14 bg-primary text-on-primary rounded-2xl shadow-lg flex items-center justify-center active:scale-90 hover:opacity-90 transition-all z-40">
+            className="fixed bottom-6 right-6 w-14 h-14 bg-secondary-fixed-dim text-on-secondary-fixed-dim rounded-2xl shadow-lg flex items-center justify-center active:scale-90 hover:opacity-90 transition-all z-40">
             <span className="material-symbols-outlined text-[32px]">{icon}</span>
         </button>
     )
